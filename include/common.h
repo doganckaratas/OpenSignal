c6a384726524a64b27b01e5ea704cfee50339499
Dogan Can Karatas <dogan.karatas@airties.com>
2018-04-20 17:14:22 +0300

    Introduced new Vector data type (stub)

--- include/common.h
+++ include/common.h
@@ -1,12 +1,12 @@
 #ifndef __COMMON_H
 #define __COMMON_H
 
-typedef struct Polar {
+typedef struct Polar_s {
 	double radius;
 	double angle;
 } Polar_t;
 
-typedef struct Complex {
+typedef struct Complex_s {
 	double real;
 	double imag;
 } Complex_t;
@@ -20,5 +20,11 @@ typedef struct Matrix_s {
 	   these fields in .c file 
 	 */
 } Matrix_t;
+
+typedef struct Vector_s {
+	int size;
+	int orientation; /* needs to be anonymous ENUM */
+	double *data;
+} Vector_t;
 
 #endif /* __COMMON_H */
---
+++ include/vector.h
@@ -0,0 +1,17 @@
+#ifndef __VECTOR_H
+#define __VECTOR_H
+
+#if 0
+Vector_t * vector_new(void);
+int	vector_size(Vector_t *v, int num, int orientation);
+int 	vector_get_elem(Vector_t *v, int num);
+int	vector_set_elem(Vector_t *v, int num);
+int	vector_from_scalar(Vector_t *v, double *scalar, int orientation);
+int	vector_to_scalar(Vector_t *v, double *scalar, int orientation);
+int	vector_set_orientation(Vector_t *v, int orientation);
+int	vector_add(Vector_t *v1, Vector_t *v2);
+int	vector_subtract(Vector_t *v1, Vector_t *v2);
+int	vector_multiply(Vector_t *v1, Vector_t *v2);
+#endif
+
+#endif /* __VECTOR_H */
